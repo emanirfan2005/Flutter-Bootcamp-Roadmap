dc473a5b05505aa1eaf0afee549b6b452af60670
emanirfan2005 <emanirfan260305@gmail.com>
2025-07-10 17:29:38 +0500

    Adding Fonts

--- Chapter_2/lib/main.dart
+++ Chapter_2/lib/main.dart
@@ -14,43 +14,35 @@ class Myapp extends StatelessWidget {
         backgroundColor: Colors.blue,
         appBar: AppBar(
           backgroundColor: Colors.redAccent,
-          title: const Text('My first project'),
+          title: const Text('My first project', style: TextStyle(fontSize: 30,fontFamily: "Pacifico",fontWeight: FontWeight.w400)),
         ),
         body: SafeArea(
           child: Row(
             children: [
-              const Text("First widget"),
+              const Text("First widget",style: TextStyle(color: Colors.red,decoration: TextDecoration.underline, fontSize: 20),),
 
-              SizedBox(
-                width: 5,
-              ),
+              SizedBox(width: 5),
               Container(
                 height: 100,
                 width: 100,
                 color: Colors.pink,
                 child: const Center(child: Text("Container 1")),
               ),
-              SizedBox(
-                width: 5,
-              ),
+              SizedBox(width: 5),
               Container(
                 height: 100,
                 width: 100,
                 color: Colors.pink,
                 child: const Center(child: Text("Container 1")),
               ),
-              SizedBox(
-                width: 5,
-              ),
+              SizedBox(width: 5),
               Container(
                 height: 100,
                 width: 100,
                 color: Colors.pink,
                 child: const Center(child: Text("Container 2")),
               ),
-              SizedBox(
-                width: 5,
-              ),
+              SizedBox(width: 5),
             ],
           ),
         ),
