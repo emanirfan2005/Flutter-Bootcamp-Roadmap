0e3c5373ec37ab983ead4df9160a31cd4bf84e3d
emanirfan2005 <emanirfan260305@gmail.com>
2025-07-13 20:35:29 +0500

    Understanding of variables and data types

--- chapter_3/lib/main.dart
+++ chapter_3/lib/main.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Myapp());
+  runApp( Myapp());
 }
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+class Myapp extends StatefulWidget {
+   Myapp({super.key});
 
   @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  int x=0;
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Text("Eman Irfan Ahmed"),
+        appBar: AppBar(
+          title: Text("My First App"),
+          backgroundColor: Colors.deepOrange,
+        ),
+        body:
+        SafeArea(child: Center(child: Text(x.toString(),style: TextStyle(fontSize: 50),))),
+        backgroundColor: Colors.tealAccent,
+        floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              x++;
+              setState(() {
+
+              });
+              print(x.toString());
+
+            },
+          child: Icon(Icons.add),
+            ),
       ),
     );
+
   }
 }
